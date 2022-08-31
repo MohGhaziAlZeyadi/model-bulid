3209e71600b5c8e8b68d838f432f134e7b1199ca
moh.al-zeyadi@cognitran.com <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>
2022-08-31 09:50:25 +0000

    Commit

--- pipelines/abalone/.ipynb_checkpoints/pipeline-checkpoint.py
+++ pipelines/abalone/.ipynb_checkpoints/pipeline-checkpoint.py
@@ -370,51 +370,44 @@ def get_pipeline(
     # Register model step that will be conditionally executed
     #########################################################
     
-    model_metrics = ModelMetrics(
-            model_statistics=MetricsSource(
-            s3_uri="{}/evaluation.json".format(step_eval.arguments["ProcessingOutputConfig"]["Outputs"][0]["S3Output"]["S3Uri"]),
-            content_type="application/json",
-        )
-    )
+#     model_metrics = ModelMetrics(
+#             model_statistics=MetricsSource(
+#             s3_uri="{}/evaluation.json".format(step_eval.arguments["ProcessingOutputConfig"]["Outputs"][0]["S3Output"]["S3Uri"]),
+#             content_type="application/json",
+#         )
+#     )
     
 
 
-    # Register model step that will be conditionally executed
-    step_register = RegisterModel(
-        name="RegisterAbaloneModel",
-        estimator=tf2_estimator,
-        model_data=step_train.properties.ModelArtifacts.S3ModelArtifacts,
-        content_types=["text/csv"],
-        response_types=["text/csv"],
-        inference_instances=["ml.m5.large", "ml.m5.large"],
-        transform_instances=["ml.m5.large"],
-        model_package_group_name=model_package_group_name,
-        approval_status=model_approval_status,
-        model_metrics=model_metrics,
-    )
+#     # Register model step that will be conditionally executed
+#     step_register = RegisterModel(
+#         name="RegisterAbaloneModel",
+#         estimator=tf2_estimator,
+#         model_data=step_train.properties.ModelArtifacts.S3ModelArtifacts,
+#         content_types=["text/csv"],
+#         response_types=["text/csv"],
+#         inference_instances=["ml.m5.large", "ml.m5.large"],
+#         transform_instances=["ml.m5.large"],
+#         model_package_group_name=model_package_group_name,
+#         approval_status=model_approval_status,
+#         model_metrics=model_metrics,
+#     )
+       
     
-    
-    
-
-    
-    
-
-    
-    
-    cond_lte = ConditionLessThanOrEqualTo(
-        left=JsonGet(
-            step_name=step_eval.name,
-            property_file=evaluation_report,
-            json_path="eval_accuracy"
-        ),
-        right=0.6,
-    )
-    step_cond = ConditionStep(
-        name="CheckMSEAbaloneEvaluation",
-        conditions=[cond_lte],
-        if_steps=[step_register],
-        else_steps=[],
-    )
+#     cond_lte = ConditionLessThanOrEqualTo(
+#         left=JsonGet(
+#             step_name=step_eval.name,
+#             property_file=evaluation_report,
+#             json_path="eval_accuracy"
+#         ),
+#         right=0.6,
+#     )
+#     step_cond = ConditionStep(
+#         name="CheckMSEAbaloneEvaluation",
+#         conditions=[cond_lte],
+#         if_steps=[step_register],
+#         else_steps=[],
+#     )
 
 
     # pipeline instance
@@ -428,7 +421,7 @@ def get_pipeline(
             input_data,
         ],
         #steps=[step_process, step_train, step_eval, step_cond],
-        steps=[step_process, step_train, step_eval, step_cond],
+        steps=[step_process, step_train, step_eval],
         sagemaker_session=pipeline_session,
     )
     return pipeline
